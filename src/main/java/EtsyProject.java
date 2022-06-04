63869edecdd1dc143d65f63bfaa7e208b0dbdf11
plamenmarinov <pmarinov@sbcglobal.net>
2022-06-04 14:56:25 -0500

    First push

--- src/main/java/EtsyProject.java
+++ src/main/java/EtsyProject.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class EtsyProject {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class EtsyProject {
+    @Test
+    public void validateLowToHigh() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.etsy.com");
+
+    }
+
 }
